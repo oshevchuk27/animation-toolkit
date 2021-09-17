6d3b5ab7954b8f2391f565b08aa0a25208868384
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-09-17 02:50:03 -0400

    Added moving particles

--- assignments/a1-hello/particles.cpp
+++ assignments/a1-hello/particles.cpp
@@ -9,40 +9,64 @@ class Particles : public atkui::Framework
   Particles() : atkui::Framework(atkui::Orthographic) {
   }
 
+  struct Particle {
+      vec3 position;
+      vec3 color;
+      float velocity;
+  };
+
   virtual void setup() {
-      struct Particle particle;
-      for (int i = 0; i < 10; i++) {
-          particle.position = vec3(0, i, 0);
-          particle.color = vec3(i, 0, 0);
-          particle.velocity = vec3(3, i + 5, 0);
+      Particle particle;
+      baseColor = vec3(0.2, 0.8, 0.2);
+     
+      for (int i = 0; i < 400; i++) {
+          particle.position = vec3(randX(), randY(), 0);
+          particle.color = baseColor + vec3(randValue(), randValue(), randValue());
+          particle.velocity = rand() % 100 + 40;
           v1.push_back(particle);
-      }
-         
+
+      }  
   }
 
   virtual void scene() {
-      vec3 vel;
-      vec3 currentPos;
+      
       for (int i = 0; i < v1.size(); i++) {
-          currentPos = v1[0].position + v1[0].velocity * dt();
-          setColor(vec3(0, 0, 1));
-          drawSphere(currentPos, 10);
-
+          v1[i].position = v1[i].position + v1[i].velocity * dt();
+          if (v1[i].position.x > width()){
+              v1[i].position.x = 0;
+          }
+          if (v1[i].position.y > height()) {
+              v1[i].position.y = 0;
+          }
+          setColor(v1[i].color);
+          drawSphere(v1[i].position, 10);
       }
       
   }
 
+  virtual float randValue() {
+      float randValue = -0.4 + static_cast <float> (rand()) /
+          (static_cast <float> (RAND_MAX / (0.4 + 0.4)));
+      return randValue;
+  }
+
+  virtual float randX() {
+      float randX = ((float)rand() / RAND_MAX) * width();
+      return randX;
+  }
+
+  virtual float randY() {
+      float randY = ((float)rand() / RAND_MAX) * height();
+      return randY;
+
+  }
+
 private:
-    vec3 currentPos;
-    vec3 vel;
     vector<Particle> v1;
+    vec3 baseColor;
 };
 
-struct Particle {
-    vec3 position;
-    vec3 color;
-    vec3 velocity;
-};
+
 
 int main(int argc, char** argv) {
   Particles viewer;
