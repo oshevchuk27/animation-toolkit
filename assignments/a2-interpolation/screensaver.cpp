8c87e1af7dd125ab7881b298b005c9e53233cf94
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-09-25 05:15:06 -0400

    Added trails

--- assignments/a2-interpolation/screensaver.cpp
+++ assignments/a2-interpolation/screensaver.cpp
@@ -4,6 +4,13 @@ using namespace glm;
 using namespace std;
 
 class Screensaver : public atkui::Framework {
+
+	struct Curve {
+		vec3 B0, B1, B2, B3;
+		vec3 color;
+	};
+
+
 public:
 	Screensaver() : atkui::Framework(atkui::Orthographic) {
 	}
@@ -11,103 +18,113 @@ public:
 	void setup() {
 
 		t = 0.0f;
+		timer = 0.0f;
 		baseColor = vec3(0.2, 0.8, 0.2);
 
 		// control points for curve 1
-		vec3 B0 = randCtrlPoints()[0];
-		vec3 B1 = randCtrlPoints()[1];
-		vec3 B2 = randCtrlPoints()[2];
-		vec3 B3 = randCtrlPoints()[3];
-		vec3 color = baseColor + vec3(randValue(), randValue(), randValue());
 
-		curve1.push_back(B0);
-		curve1.push_back(B1);
-		curve1.push_back(B2);
-		curve1.push_back(B3);
-		curve1.push_back(color);
+		curve1.B0 = vec3(randX(), randY(), 0);
+		curve1.B1 = vec3(randX(), randY(), 0);
+		curve1.B2 = vec3(randX(), randY(), 0);
+		curve1.B3 = vec3(randX(), randY(), 0);
+		curve1.color = baseColor + vec3(randValue(), randValue(), randValue());
 
 		// control points for curve 2
-		B0 = randCtrlPoints()[0];
-		B1 = randCtrlPoints()[1];
-		B2 = randCtrlPoints()[2];
-		B3 = randCtrlPoints()[3];
-		color = baseColor + vec3(randValue(), randValue(), randValue());
 
-
-		curve2.push_back(B0);
-		curve2.push_back(B1);
-		curve2.push_back(B2);
-		curve2.push_back(B3);
-		curve2.push_back(color);
+		curve2.B0 = vec3(randX(), randY(), 0);
+		curve2.B1 = vec3(randX(), randY(), 0);
+		curve2.B2 = vec3(randX(), randY(), 0);
+		curve2.B3 = vec3(randX(), randY(), 0);
+		curve2.color = baseColor + vec3(randValue(), randValue(), randValue());
 
 
 	}
 
 	void scene() {
 
-		vec3 B0_1 = curve1[0] * (1 - t) + curve2[0] * t;
-		vec3 B1_1 = curve1[1] * (1 - t) + curve2[1] * t;
-		vec3 B2_1 = curve1[2] * (1 - t) + curve2[2] * t;
-		vec3 B3_1 = curve1[3] * (1 - t) + curve2[3] * t;
-		vec3 color_1 = curve1[4] * (1 - t) + curve2[4] * t;
+		t += 0.04;
 
-		current.push_back(B0_1);
-		current.push_back(B1_1);
-		current.push_back(B2_1);
-		current.push_back(B3_1);
-		current.push_back(color_1);
+		// interpolated points
+
+		current.B0 = curve1.B0 * (1 - t) + curve2.B0 * t;
+		current.B1 = curve1.B1 * (1 - t) + curve2.B1 * t;
+		current.B2 = curve1.B2 * (1 - t) + curve2.B2 * t;
+		current.B3 = curve1.B3 * (1 - t) + curve2.B3 * t;
+		current.color = curve1.color * (1 - t) + curve2.color * t;
 
 
 
 		// draw curve1
 		for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
 
-			setColor(curve1[4]);
+			setColor(curve1.color);
 
 
-			vec3 p = (float)pow((1 - t), 3) * curve1[0] + 3 * t * (float)pow((1 - t), 2) * curve1[1] + (3 * (float)pow(t, 2)) * (1 - t) * curve1[2] + (float)pow(t, 3) * curve1[3];
+			vec3 p = (float)pow((1 - t), 3) * curve1.B0 + 3 * t * (float)pow((1 - t), 2) * curve1.B1
+				+ (3 * (float)pow(t, 2)) * (1 - t) * curve1.B2 + (float)pow(t, 3) * curve1.B3;
 
-			drawSphere(p, 5);
+			drawSphere(p, 1.0);
 		}
 
 		// draw curve2
 		for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
 
-			setColor(curve2[4]);
+			setColor(curve2.color);
 
 
-			vec3 p = (float)pow((1 - t), 3) * curve2[0] + 3 * t * (float)pow((1 - t), 2) * curve2[1] + (3 * (float)pow(t, 2)) * (1 - t) * curve2[2] + (float)pow(t, 3) * curve2[3];
+			vec3 p = (float)pow((1 - t), 3) * curve2.B0 + 3 * t * (float)pow((1 - t), 2) * curve2.B1 +
+				(3 * (float)pow(t, 2)) * (1 - t) * curve2.B2 + (float)pow(t, 3) * curve2.B3;
 
-			drawSphere(p, 5);
+			drawSphere(p, 1.0);
 		}
 
 
-
-		t += dt();
 
 
 		if (t > 1) {
-			t = 0;
-			curve1[0] = curve2[0];
-			curve1[1] = curve2[1];
-			curve1[2] = curve2[2];
-			curve1[3] = curve2[3];
-			curve1[4] = curve2[4];
-			curve2 = randCtrlPoints();
 
+			t = 0;
+			curve1.B0 = curve2.B0;
+			curve1.B1 = curve2.B1;
+			curve1.B2 = curve2.B2;
+			curve1.B3 = curve2.B3;
+			curve1.color = curve2.color;
+			curve2 = randCurve();
 
 		}
 
 
-		// draw current curve
-		for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
+		timer += dt();
 
-			setColor(color_1);
+		if (timer > 0.3f) {
+			timer = 0.0;
+
+			curves.push_back(current);
+
+		}
+
+		if (curves.size() > 50.0) {
+			curves.pop_front();
+		}
+
+		for (Curve& curve : curves) {
 
 
-			vec3 p = (float)pow((1 - t), 3) * B0_1 + 3 * t * (float)pow((1 - t), 2) * B1_1 + (3 * (float)pow(t, 2)) * (1 - t) * B2_1 + (float)pow(t, 3) * B3_1;
 
-			drawSphere(p, 5);
+			// draw current curve
+			for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
+
+				setColor(curve.color);
+
+
+				vec3 p = (float)pow((1 - t), 3) * curve.B0 + 3 * t * (float)pow((1 - t), 2) * curve.B1
+					+ (3 * (float)pow(t, 2)) * (1 - t) * curve.B2 + (float)pow(t, 3) * curve.B3;
+
+				drawSphere(p, 1.0);
+			}
+
+
+
 		}
 
 
@@ -126,31 +143,36 @@ public:
 	}
 
 	virtual float randValue() {
-		float randValue = -2 + static_cast <float> (rand()) /
-			(static_cast <float> (RAND_MAX / (2 + 2)));
+		float randValue = -10 + static_cast <float> (rand()) /
+			(static_cast <float> (RAND_MAX / (10 + 10)));
 		return randValue;
 	}
 
 	// generate random control points
-	virtual std::vector<vec3> randCtrlPoints() {
-		vector<vec3> v;
-		for (int i = 0; i < 4; i++) {
-			v.push_back(vec3(randX(), randY(), 0));
-		}
+	virtual Curve randCurve() {
+		Curve randCurve;
 
-		vec3 color = baseColor + vec3(randValue(), randValue(), randValue());
-		v.push_back(color);
+		randCurve.B0 = vec3(randX(), randY(), 0);
+		randCurve.B1 = vec3(randX(), randY(), 0);
+		randCurve.B2 = vec3(randX(), randY(), 0);
+		randCurve.B3 = vec3(randX(), randY(), 0);
 
-		return v;
+
+		randCurve.color = baseColor + vec3(randValue(), randValue(), randValue());
+
+
+		return randCurve;
 
 	}
 
 private:
-	std::vector<vec3> curve1;
-	std::vector<vec3> curve2;
-	std::vector<vec3> current;
+	Curve curve1;
+	Curve curve2;
+	Curve current;
 	float t;
+	float timer;
 	vec3 baseColor;
+	std::list<Curve> curves;
 
 };
 
@@ -158,4 +180,3 @@ int main(int argc, char** argv) {
 	Screensaver viewer;
 	viewer.run();
 }
-
