af0e59d6c6f7a9c63ac2cc6b1f3fbf5487005916
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-09-25 04:29:08 -0400

    Cleaned up code

--- assignments/a2-interpolation/particlecubic.cpp
+++ assignments/a2-interpolation/particlecubic.cpp
@@ -2,45 +2,47 @@
 using namespace glm;
 
 class ParticleCubic : public atkui::Framework {
- public:
-  ParticleCubic() : atkui::Framework(atkui::Orthographic) {
-  }
+public:
+	ParticleCubic() : atkui::Framework(atkui::Orthographic) {
+	}
 
-  void setup() {
-      t = 0.0f;
-  }
+	void setup() {
+		t = 0.0f;
+	}
 
-  void scene() {
-    
-      float duration = 5.0f;
+	void scene() {
 
-      (float)t = fmod(elapsedTime(), 5);
-      t = t / duration;
- 
+		float duration = 5.0f;
 
-      vec3 p = (float)pow((1 - t), 3) * B0 + 3 * t * (float)pow((1 - t), 2) * B1 + (3 * (float)pow(t, 2)) * (1 - t) * B2 + (float)pow(t, 3) * B3;
-      setColor(vec3(1));
-      drawSphere(p, 10);
+		(float)t = fmod(elapsedTime(), 5);
+		t = t / duration;
 
-      for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
-          setColor(vec3(0, 0, 1));
-          vec3 p = (float)pow((1 - t), 3) * B0 + 3 * t * (float)pow((1 - t), 2) * B1 + (3 * (float)pow(t, 2)) * (1 - t) * B2 + (float)pow(t, 3) * B3;
-          drawSphere(p, 5);
-    }
-    
-  }
+
+		vec3 p = (float)pow((1 - t), 3) * B0 + 3 * t * (float)pow((1 - t), 2) * B1
+			+ (3 * (float)pow(t, 2)) * (1 - t) * B2 + (float)pow(t, 3) * B3;
+		setColor(vec3(1));
+		drawSphere(p, 10);
+
+		for (float t = 0.0f; t <= 1.0f; t += 0.001f) {
+			setColor(vec3(0, 0, 1));
+			vec3 p = (float)pow((1 - t), 3) * B0 + 3 * t * (float)pow((1 - t), 2) * B1
+				+ (3 * (float)pow(t, 2)) * (1 - t) * B2 + (float)pow(t, 3) * B3;
+			drawSphere(p, 2);
+		}
+
+	}
 
 private:
-    float t; 
-    vec3 B0 = vec3(100, 50, 0);
-    vec3 B1 = vec3(150, 200, 0);
-    vec3 B2 = vec3(250, 100, 0);
-    vec3 B3 = vec3(300, 300, 0);
+	float t;
+	vec3 B0 = vec3(100, 50, 0);
+	vec3 B1 = vec3(150, 200, 0);
+	vec3 B2 = vec3(250, 100, 0);
+	vec3 B3 = vec3(300, 300, 0);
 
 };
 
 int main(int argc, char** argv) {
-  ParticleCubic viewer;
-  viewer.run();
+	ParticleCubic viewer;
+	viewer.run();
 }
 
