34a045ff1d0889c9133b720fb65d5f8d4bd25d4a
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-09-29 17:23:49 -0400

    Made changes to linear interpolator

--- assignments/a3-splines/interpolator_linear.h
+++ assignments/a3-splines/interpolator_linear.h
@@ -14,12 +14,23 @@ class InterpolatorLinear : public Interpolator
 public:
     InterpolatorLinear() : Interpolator("Linear") {}
     virtual glm::vec3 interpolate(int segment, double u) const {
+
        // todo: your code here
-       return glm::vec3(0);
+
+        glm::vec3 p = getControlPoint(segment) * (float) (1 - u) + getControlPoint(segment + 1) * (float) u;
+
+        return p;
+
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
+       
        // todo: your code here
+        for (int i = 0; i < keys.size(); i++) {
+            mCtrlPoints.push_back(keys[i]);
+        }
+       
+
     }
 };
 
