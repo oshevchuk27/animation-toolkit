9b1bde546ad118cd18cbb8fc3b38c8c025f88ab5
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-09-30 20:32:35 -0400

    added draft of hermite

--- assignments/a3-splines/interpolator_hermite.h
+++ assignments/a3-splines/interpolator_hermite.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <Eigen/Dense>
 #include "interpolator.h"
+#include "agl/aglm.h"
 
 //--------------------------------
 // Hermite 
@@ -21,8 +22,81 @@ public:
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
-       // todo: your code here
+        // todo: your code here
+
+        Eigen::MatrixXd A(keys.size(), keys.size());
+        Eigen::MatrixXd p(keys.size(), 3);
+        Eigen::MatrixXd pPrime(keys.size(), 3); // slopes for each control point
+
+        if (mIsClamped) {
+            for (int i = 0; i < keys.size(); i++) {
+                for (int j = 0; j < keys.size(); j++) {
+                    if (i == 0) {
+                        if (j == 0) {
+                            A(i, j) = 1;
+                        }
+                        else {
+                            A(i, j) = 0;
+                        }
+                    }
+                    else if (i == keys.size() - 1) {
+                        if (j == keys.size() - 1) {
+                            A(i, j) = 1;
+                        }
+                        else {
+                            A(i, j) = 0;
+                        }
+                    }
+                    else {
+                        if (j == i - 1) {
+                            A(i, j) = 1;
+                        }
+                        else if (j == i) {
+                            A(i, j) = 4;
+                        }
+                        else if (j == i + 1) {
+                            A(i, j) = 1;
+                        }
+                        else {
+                            A(i, j) = 0;
+                        }
+
+                    }
+
+
+                }
+            }
+
+            for (int i = 0; i < keys.size(); i++) {
+                for (int j = 0; j < 3; j++) {
+                    // hardcoding the first and last slopes
+                    if (i == 0 || i == keys.size() - 1) {
+                        p(i, j) = mClampDir[j];
+                    }
+                    else {
+                        p(i, j) = (3.0f * (keys[i + 1] - keys[i - 1]))[j];
+                    }
+
+                }
+
+            }
+
+
+
+            pPrime = A.inverse() * p;
+
+            for (int i = 0; i < keys.size(); i++) {
+                mCtrlPoints.push_back(keys[i]);
+                mCtrlPoints.push_back(glm::vec3(pPrime(i, 0), pPrime(i, 1), pPrime(i, 2)));
+            }
+
+
+        }
+
     }
+        
+       
+    
 
     void setClamped(bool c) { mIsClamped = c; }
     bool isClamped() const { return mIsClamped; }
