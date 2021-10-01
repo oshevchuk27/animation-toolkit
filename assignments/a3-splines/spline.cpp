22e3ae5dc84df8dcb378d80dac1b4d5bb38fad9d
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-10-01 13:51:58 -0400

    added draft of spline implementation

--- assignments/a3-splines/spline.cpp
+++ assignments/a3-splines/spline.cpp
@@ -102,9 +102,54 @@ glm::vec3 Spline::getValue(float t) const {
     mDirty = false;
   }
 
+  glm::vec3 value;
+
   // todo: your code here
   // compute the segment containing t
   // compute the value [0, 1] along the segment for interpolation
-  return glm::vec3(0); 
+ // if (t < getTime(0)) {
+      //value = mKeys[0];
+ // }
+  //else if (t > getTime((int)mKeys.size() - 1)) {
+      //value = mKeys[mKeys.size() - 1];
+  //}
+  std::cout << mKeys.size() << std::endl;
+
+  if (mKeys.size() == 0) {
+      value = glm::vec3(0);
+  }
+  else {
+      if (t < getTime(0)) {
+          value = mKeys[0];
+      }
+      else if (t > getTime(mKeys.size() - 1)) {
+          value = mKeys[mKeys.size() - 1];
+      }
+      else {
+          int segment = 0;
+          float u = 0;
+          for (int i = 0; i < mKeys.size() - 1; i++) {
+              if (t >= getTime(i) && t <= getTime(i + 1)) {
+                  segment = i;
+                  // computing normalized time
+                  u = (t - getTime(i)) / (getTime(i + 1) - getTime(i));
+                  //if (getInterpolationType() == "Linear") {
+                      //value = gDefaultInterpolator.interpolate(segment, t);
+                      //value = mKeys[i] * (float)(1 - u) + mKeys[i + 1] * (float)u;
+                  //}
+                 
+
+              }
+          }
+
+
+          value = mInterpolator -> interpolate(segment, u);
+      }
+
+      
+
+  }
+  
+  return value; 
 }
 
