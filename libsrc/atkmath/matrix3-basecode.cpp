4190620d53106ee518b3a75f8a358e9c45555b63
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-10-22 16:56:46 -0400

    Cleaned up code

--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -4,244 +4,239 @@
 namespace atkmath {
 
 
-Matrix3 computeRx(double alpha) {
-	Matrix3 mRx;
+	Matrix3 computeRx(double alpha) {
+		Matrix3 mRx;
 
-	mRx[0][0] = 1;
-	mRx[0][1] = 0;
-	mRx[0][2] = 0;
-	mRx[1][0] = 0;
-	mRx[1][1] = cos(alpha);
-	mRx[1][2] = -sin(alpha);
-	mRx[2][0] = 0;
-	mRx[2][1] = sin(alpha);
-	mRx[2][2] = cos(alpha);
+		mRx[0][0] = 1;
+		mRx[0][1] = 0;
+		mRx[0][2] = 0;
+		mRx[1][0] = 0;
+		mRx[1][1] = cos(alpha);
+		mRx[1][2] = -sin(alpha);
+		mRx[2][0] = 0;
+		mRx[2][1] = sin(alpha);
+		mRx[2][2] = cos(alpha);
 
-	return mRx;
+		return mRx;
 
-
-}
-
-Matrix3 computeRy(double theta) {
-	Matrix3 mRy;
-
-	mRy[0][0] = cos(theta);
-	mRy[0][1] = 0;
-	mRy[0][2] = sin(theta);
-	mRy[1][0] = 0;
-	mRy[1][1] = 1;
-	mRy[1][2] = 0;
-	mRy[2][0] = -sin(theta);
-	mRy[2][1] = 0;
-	mRy[2][2] = cos(theta);
-
-	return mRy;
-
-}
-
-Matrix3 computeRz(double beta) {
-	Matrix3 mRz;
-
-	mRz[0][0] = cos(beta);
-	mRz[0][1] = -sin(beta);
-	mRz[0][2] = 0;
-	mRz[1][0] = sin(beta);
-	mRz[1][1] = cos(beta);
-	mRz[1][2] = 0;
-	mRz[2][0] = 0;
-	mRz[2][1] = 0;
-	mRz[2][2] = 1;
-
-	return mRz;
-}
-
-Vector3 Matrix3::toEulerAnglesXYZ() const
-{
-	Matrix3 m_xyz = *this;
-
-	double alpha, beta;
-	double theta = asin(m_xyz[0][2]);
-
-	if (theta == PI / 2 || theta == - PI / 2) {
-		beta = 0;
-		alpha = atan2(m_xyz[2][1], m_xyz[1][1]);
-	}
-	else {
-		beta = -atan2(m_xyz[0][1], m_xyz[0][0]);
-		alpha = -atan2(m_xyz[1][2], m_xyz[2][2]);
-	}
-   return Vector3(alpha, theta, beta);
-}
-
-Vector3 Matrix3::toEulerAnglesXZY() const
-{
-	Matrix3 m_xzy = *this;
-	double beta = -asin(m_xzy[0][1]);
-	double alpha, theta;
-
-	if (beta == PI / 2 || beta == - PI / 2) {
-		theta = 0;
-		alpha = -atan2(m_xzy[1][2], m_xzy[2][2]);
-	}
-
-	else {
-		alpha = atan2(m_xzy[2][1], m_xzy[1][1]);
-		theta = atan2(m_xzy[0][2], m_xzy[0][0]);
-
-	}
-	
-	return Vector3(alpha, theta, beta);
-}
-
-Vector3 Matrix3::toEulerAnglesYXZ() const
-{
-	Matrix3 m_yxz = *this;
-
-	double alpha = -asin(m_yxz[1][2]);
-	double beta, theta;
-
-	if (alpha == PI / 2) {
-		theta = 0;
-		beta = atan2(m_yxz[2][0], m_yxz[2][1]);
-	}
-	else if (alpha == -PI / 2) {
-		theta = 0;
-		beta = -atan2(m_yxz[0][1], m_yxz[0][0]);
-	}
-	else {
-		beta = atan2(m_yxz[1][0], m_yxz[1][1]);
-		theta = atan2(m_yxz[0][2], m_yxz[2][2]);
-	}
-   return Vector3(alpha, theta, beta);
-}
-
-Vector3 Matrix3::toEulerAnglesYZX() const
-{
-	Matrix3 m_yzx = *this;
-
-	double beta = asin(m_yzx[1][0]);
-	double alpha, theta;
-
-	if (beta == PI / 2 || beta == -PI / 2) {
-		alpha = 0;
-		theta = atan2(m_yzx[0][2], m_yzx[2][2]);
-	}
-	else {
-
-		alpha = -atan2(m_yzx[1][2], m_yzx[1][1]);
-		theta = -atan2(m_yzx[2][0], m_yzx[0][0]);
 
 	}
 
+	Matrix3 computeRy(double theta) {
+		Matrix3 mRy;
 
-   return Vector3(alpha, theta, beta);
-}
+		mRy[0][0] = cos(theta);
+		mRy[0][1] = 0;
+		mRy[0][2] = sin(theta);
+		mRy[1][0] = 0;
+		mRy[1][1] = 1;
+		mRy[1][2] = 0;
+		mRy[2][0] = -sin(theta);
+		mRy[2][1] = 0;
+		mRy[2][2] = cos(theta);
 
-Vector3 Matrix3::toEulerAnglesZXY() const
-{
-	Matrix3 m_zxy = *this;
-	double alpha = asin(m_zxy[2][1]);
-	double beta, theta;
-
-	if (alpha == PI / 2) {
-		beta = 0;
-		theta = atan2(m_zxy[0][2], m_zxy[0][0]);
-
-	}
-	else if (alpha == -PI / 2) {
-		beta = 0;
-		theta = atan2(m_zxy[0][2], m_zxy[1][2]);
-
-	}
-	else {
-
-		beta = -atan2(m_zxy[0][1], m_zxy[1][1]);
-		theta = -atan2(m_zxy[2][0], m_zxy[2][2]);
+		return mRy;
 
 	}
 
-	
-   return Vector3(alpha, theta, beta);
-}
+	Matrix3 computeRz(double beta) {
+		Matrix3 mRz;
 
-Vector3 Matrix3::toEulerAnglesZYX() const
-{
-	Matrix3 m_zyx = *this;
+		mRz[0][0] = cos(beta);
+		mRz[0][1] = -sin(beta);
+		mRz[0][2] = 0;
+		mRz[1][0] = sin(beta);
+		mRz[1][1] = cos(beta);
+		mRz[1][2] = 0;
+		mRz[2][0] = 0;
+		mRz[2][1] = 0;
+		mRz[2][2] = 1;
 
-	double alpha, beta;
-
-	double theta = - asin(m_zyx[2][0]);
-
-	if (theta == PI / 2) {
-		beta = 0;
-		alpha = atan2(m_zyx[0][1], m_zyx[0][2]);
+		return mRz;
 	}
-	else if (theta == -PI / 2) {
-		beta = 0;
-		alpha = -atan2(m_zyx[0][1], m_zyx[1][1]);
+
+	Vector3 Matrix3::toEulerAnglesXYZ() const
+	{
+		Matrix3 m_xyz = *this;
+
+		double alpha, beta;
+		double theta = asin(m_xyz[0][2]);
+
+		if (theta == PI / 2 || theta == -PI / 2) {
+			beta = 0;
+			alpha = atan2(m_xyz[2][1], m_xyz[1][1]);
+		}
+		else {
+			beta = -atan2(m_xyz[0][1], m_xyz[0][0]);
+			alpha = -atan2(m_xyz[1][2], m_xyz[2][2]);
+		}
+		return Vector3(alpha, theta, beta);
 	}
-	else {
 
-		beta = atan2(m_zyx[1][0], m_zyx[0][0]);
-		alpha = atan2(m_zyx[2][1], m_zyx[2][2]);
+	Vector3 Matrix3::toEulerAnglesXZY() const
+	{
+		Matrix3 m_xzy = *this;
+		double beta = -asin(m_xzy[0][1]);
+		double alpha, theta;
+
+		if (beta == PI / 2 || beta == -PI / 2) {
+			theta = 0;
+			alpha = -atan2(m_xzy[1][2], m_xzy[2][2]);
+		}
+
+		else {
+			alpha = atan2(m_xzy[2][1], m_xzy[1][1]);
+			theta = atan2(m_xzy[0][2], m_xzy[0][0]);
+
+		}
+
+		return Vector3(alpha, theta, beta);
+	}
+
+	Vector3 Matrix3::toEulerAnglesYXZ() const
+	{
+		Matrix3 m_yxz = *this;
+
+		double alpha = -asin(m_yxz[1][2]);
+		double beta, theta;
+
+		if (alpha == PI / 2) {
+			theta = 0;
+			beta = atan2(m_yxz[2][0], m_yxz[2][1]);
+		}
+		else if (alpha == -PI / 2) {
+			theta = 0;
+			beta = -atan2(m_yxz[0][1], m_yxz[0][0]);
+		}
+		else {
+			beta = atan2(m_yxz[1][0], m_yxz[1][1]);
+			theta = atan2(m_yxz[0][2], m_yxz[2][2]);
+		}
+		return Vector3(alpha, theta, beta);
+	}
+
+	Vector3 Matrix3::toEulerAnglesYZX() const
+	{
+		Matrix3 m_yzx = *this;
+
+		double beta = asin(m_yzx[1][0]);
+		double alpha, theta;
+
+		if (beta == PI / 2 || beta == -PI / 2) {
+			alpha = 0;
+			theta = atan2(m_yzx[0][2], m_yzx[2][2]);
+		}
+		else {
+
+			alpha = -atan2(m_yzx[1][2], m_yzx[1][1]);
+			theta = -atan2(m_yzx[2][0], m_yzx[0][0]);
+
+		}
+
+
+		return Vector3(alpha, theta, beta);
+	}
+
+	Vector3 Matrix3::toEulerAnglesZXY() const
+	{
+		Matrix3 m_zxy = *this;
+		double alpha = asin(m_zxy[2][1]);
+		double beta, theta;
+
+		if (alpha == PI / 2) {
+			beta = 0;
+			theta = atan2(m_zxy[0][2], m_zxy[0][0]);
+
+		}
+		else if (alpha == -PI / 2) {
+			beta = 0;
+			theta = atan2(m_zxy[0][2], m_zxy[1][2]);
+
+		}
+		else {
+
+			beta = -atan2(m_zxy[0][1], m_zxy[1][1]);
+			theta = -atan2(m_zxy[2][0], m_zxy[2][2]);
+
+		}
+
+
+		return Vector3(alpha, theta, beta);
+	}
+
+	Vector3 Matrix3::toEulerAnglesZYX() const
+	{
+		Matrix3 m_zyx = *this;
+
+		double alpha, beta;
+
+		double theta = -asin(m_zyx[2][0]);
+
+		if (theta == PI / 2) {
+			beta = 0;
+			alpha = atan2(m_zyx[0][1], m_zyx[0][2]);
+		}
+		else if (theta == -PI / 2) {
+			beta = 0;
+			alpha = -atan2(m_zyx[0][1], m_zyx[1][1]);
+		}
+		else {
+
+			beta = atan2(m_zyx[1][0], m_zyx[0][0]);
+			alpha = atan2(m_zyx[2][1], m_zyx[2][2]);
+
+		}
+		return Vector3(alpha, theta, beta);
+	}
+
+	void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
+	{
+
+		*this = computeRx(angleRad[0]) * computeRy(angleRad[1]) * computeRz(angleRad[2]);
+	}
+
+	void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
+	{
+		*this = computeRx(angleRad[0]) * computeRz(angleRad[2]) * computeRy(angleRad[1]);
+	}
+
+	void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
+	{
+		*this = computeRy(angleRad[1]) * computeRx(angleRad[0]) * computeRz(angleRad[2]);
+	}
+
+	void Matrix3::fromEulerAnglesYZX(const Vector3& angleRad)
+	{
+		*this = computeRy(angleRad[1]) * computeRz(angleRad[2]) * computeRx(angleRad[0]);
+	}
+
+	void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
+	{
+		*this = computeRz(angleRad[2]) * computeRx(angleRad[0]) * computeRy(angleRad[1]);
+	}
+
+	void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
+	{
+
+		*this = computeRz(angleRad[2]) * computeRy(angleRad[1]) * computeRx(angleRad[0]);
+	}
+
+
+	void Matrix3::toAxisAngle(Vector3& axis, double& angleRad) const
+	{
+		Matrix3 matrix = *this;
+		Quaternion q;
+		q.fromMatrix(matrix);
+		q.toAxisAngle(axis, angleRad);
 
 	}
-   return Vector3(alpha, theta, beta);
-}
 
-void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
-{
-
-   *this = computeRx(angleRad[0]) * computeRy(angleRad[1]) * computeRz(angleRad[2]);
-}
-
-void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
-{
-   *this = computeRx(angleRad[0]) * computeRz(angleRad[2]) * computeRy(angleRad[1]);
-}
-
-void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
-{
-   *this = computeRy(angleRad[1]) * computeRx(angleRad[0]) * computeRz(angleRad[2]);
-}
-
-void Matrix3::fromEulerAnglesYZX(const Vector3& angleRad)
-{
-   *this = computeRy(angleRad[1]) * computeRz(angleRad[2]) * computeRx(angleRad[0]);
-}
-
-void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
-{
-   *this = computeRz(angleRad[2]) * computeRx(angleRad[0]) * computeRy(angleRad[1]);
-}
-
-void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
-{
-
-   *this = computeRz(angleRad[2]) * computeRy(angleRad[1]) * computeRx(angleRad[0]);
-}
-
-
-void Matrix3::toAxisAngle(Vector3& axis, double& angleRad) const
-{
-	Matrix3 matrix = *this;
-
-
-   
-}
-
-void Matrix3::fromAxisAngle(const Vector3& axis, double angleRad)
-{
-	Quaternion q;
-	q[0] = sin(angleRad / 2) * axis[0];
-	q[1] = sin(angleRad / 2) * axis[1];
-	q[2] = sin(angleRad / 2) * axis[2];
-	q[3] = cos(angleRad / 2);
-	q.normalize();
-
-
-   *this = q.toMatrix();
-}
+	void Matrix3::fromAxisAngle(const Vector3& axis, double angleRad)
+	{
+		Quaternion q;
+		q.fromAxisAngle(axis, angleRad);
+		*this = q.toMatrix();
+	}
 
 
 
--- libsrc/atkmath/quaternion-basecode.cpp
+++ libsrc/atkmath/quaternion-basecode.cpp
@@ -4,53 +4,97 @@
 
 namespace atkmath {
 
-Quaternion Quaternion::Slerp(const Quaternion& q0, const Quaternion& q1, double t)
-{
-	// TODO
-	return Quaternion(1,0,0,0);
-}
+	Quaternion Quaternion::Slerp(const Quaternion& q0, const Quaternion& q1, double t)
+	{
+		// TODO
+		return Quaternion(1, 0, 0, 0);
+	}
 
-void Quaternion::toAxisAngle (Vector3& axis, double& angleRad) const
-{
-	angleRad = 2 * acos(mW);
-	axis[0] = mX / (sin(angleRad / 2));
-	axis[1] = mY / (sin(angleRad / 2));
-	axis[2] = mZ / (sin(angleRad / 2));
+	void Quaternion::toAxisAngle(Vector3& axis, double& angleRad) const
+	{
+		angleRad = 2 * acos(mW);
+		axis[0] = mX / (sin(angleRad / 2));
+		axis[1] = mY / (sin(angleRad / 2));
+		axis[2] = mZ / (sin(angleRad / 2));
 
-}
+	}
 
-void Quaternion::fromAxisAngle (const Vector3& axis, double angleRad)
-{
-	mX = sin(angleRad / 2) * axis[0];
-	mY = sin(angleRad / 2) * axis[1];
-	mZ = sin(angleRad / 2) * axis[2];
-	mW = cos(angleRad / 2);
-	normalize();
-}
+	void Quaternion::fromAxisAngle(const Vector3& axis, double angleRad)
+	{
+		mX = sin(angleRad / 2) * axis[0];
+		mY = sin(angleRad / 2) * axis[1];
+		mZ = sin(angleRad / 2) * axis[2];
+		mW = cos(angleRad / 2);
+		normalize();
+	}
 
-Matrix3 Quaternion::toMatrix () const
-{
+	Matrix3 Quaternion::toMatrix() const
+	{
 
-	Matrix3 matrix;
+		Matrix3 matrix;
 
-	matrix[0][0] = 1 - 2 * (pow(mY, 2) + pow(mZ, 2));
-	matrix[0][1] = 2 * (mX * mY - mW * mZ);
-	matrix[0][2] = 2 * (mX * mZ + mW * mY);
-	matrix[1][0] = 2 * (mX * mY + mW * mZ);
-	matrix[1][1] = 1 - 2 * (pow(mX, 2) + pow(mZ, 2));
-	matrix[1][2] = 2 * (mY * mZ - mW * mX);
-	matrix[2][0] = 2 * (mX * mZ - mW * mY);
-	matrix[2][1] = 2 * (mY * mZ + mW * mX);
-	matrix[2][2] = 1 - 2 * (pow(mX, 2) + pow(mY, 2));
-	
+		matrix[0][0] = 1 - 2 * (pow(mY, 2) + pow(mZ, 2));
+		matrix[0][1] = 2 * (mX * mY - mW * mZ);
+		matrix[0][2] = 2 * (mX * mZ + mW * mY);
+		matrix[1][0] = 2 * (mX * mY + mW * mZ);
+		matrix[1][1] = 1 - 2 * (pow(mX, 2) + pow(mZ, 2));
+		matrix[1][2] = 2 * (mY * mZ - mW * mX);
+		matrix[2][0] = 2 * (mX * mZ - mW * mY);
+		matrix[2][1] = 2 * (mY * mZ + mW * mX);
+		matrix[2][2] = 1 - 2 * (pow(mX, 2) + pow(mY, 2));
 
 
-	return matrix;
-}
 
-void Quaternion::fromMatrix(const Matrix3& rot)
-{
-	float x = sqrt((1 / 4.0) * (1 + rot[0][0] - rot[1][1] - rot[2][2]));
-}
+		return matrix;
+	}
+
+	void Quaternion::fromMatrix(const Matrix3& rot)
+	{
+		float xSquare = (1 / 4.0) * (1 + rot[0][0] - rot[1][1] - rot[2][2]);
+		float ySquare = (1 / 4.0) * (1 - rot[0][0] + rot[1][1] - rot[2][2]);
+		float zSquare = (1 / 4.0) * (1 - rot[0][0] - rot[1][1] + rot[2][2]);
+		float wSquare = (1 / 4.0) * (rot[0][0] + rot[1][1] + rot[2][2] + 1);
+
+		float max;
+
+		if (xSquare > ySquare && xSquare > zSquare && xSquare > wSquare) {
+			max = xSquare;
+		}
+		else if (ySquare > xSquare && ySquare > zSquare && ySquare > wSquare) {
+			max = ySquare;
+		}
+		else if (zSquare > xSquare && zSquare > ySquare && zSquare > wSquare) {
+			max = zSquare;
+		}
+		else if (wSquare > xSquare && wSquare > ySquare && wSquare > zSquare) {
+			max = wSquare;
+		}
+
+		if (xSquare == max) {
+			mX = sqrt(xSquare);
+			mW = 1 / (4 * mX) * (rot[2][1] - rot[1][2]);
+			mZ = 1 / (4 * mW) * (rot[1][0] - rot[0][1]);
+			mY = 1 / (4 * mW) * (rot[0][2] - rot[2][0]);
+		}
+		else if (ySquare == max) {
+			mY = sqrt(ySquare);
+			mW = 1 / (4 * mY) * (rot[0][2] - rot[2][0]);
+			mX = 1 / (4 * mW) * (rot[2][1] - rot[1][2]);
+			mZ = 1 / (4 * mW) * (rot[1][0] - rot[0][1]);
+		}
+		else if (zSquare == max) {
+			mZ = sqrt(zSquare);
+			mX = 1 / (4 * mZ) * (rot[0][2] - rot[2][0]);
+			mY = 1 / (4 * mX) * (rot[1][0] + rot[0][1]);
+			mW = 1 / (4 * mX) * (rot[2][1] - rot[1][2]);
+		}
+		else {
+			mW = sqrt(wSquare);
+			mX = 1 / (4 * mW) * (rot[2][1] - rot[1][2]);
+			mY = 1 / (4 * mW) * (rot[0][2] - rot[2][0]);
+			mZ = 1 / (4 * mW) * (rot[1][0] - rot[0][1]);
+		}
+
+	}
 
 }
