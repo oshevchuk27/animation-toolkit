2b6687c9604c7160fca8d81d12b1157dc60e3609
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-10-30 19:20:37 -0400

    some fixes

--- assignments/a6-transform/boxes.cpp
+++ assignments/a6-transform/boxes.cpp
@@ -9,89 +9,89 @@ using glm::vec3;
 class StackingBoxes : public atkui::Framework
 {
 public:
-   StackingBoxes() : atkui::Framework(atkui::Perspective) {}
-   virtual ~StackingBoxes() {}
+	StackingBoxes() : atkui::Framework(atkui::Perspective) {}
+	virtual ~StackingBoxes() {}
 
-   void setup() {
-      lookAt(vec3(150, 150, 150), vec3(0));
-   }
+	void setup() {
+		lookAt(vec3(150, 150, 150), vec3(0));
+	}
 
-   void scene()
-   {
-      // Question 1 - B1
-      vec3 d10 = vec3(-2, 0, 1);
-      quat R10 = glm::angleAxis(0.f, vec3(1,0,0));
-      Transform F10(R10, d10); // transform from frame 1 to world
-      setColor(vec3(0,0.5,0));
-      drawBox(F10);
+	void scene()
+	{
+		// Question 1 - B1
+		vec3 d10 = vec3(-2, 0, 1);
+		quat R10 = glm::angleAxis(0.f, vec3(1, 0, 0));
+		Transform F10(R10, d10); // transform from frame 1 to world
+		setColor(vec3(0, 0.5, 0));
+		drawBox(F10);
 
-      // Question 1 - B2
-      vec3 d20 = vec3(4, 0, 2);
-      float angleInRad = 45 * atkmath::PI / 180;
-      quat R20 = glm::angleAxis(angleInRad, vec3(0,0,1));
-      Transform F20(R20, d20); // transform from frame 2 to world
-      setColor(vec3(0.5,0, 0.5));
-      drawBox(F20);
+		// Question 1 - B2
+		vec3 d20 = vec3(4, 0, 2);
+		float angleInRad = 45 * atkmath::PI / 180;
+		quat R20 = glm::angleAxis(angleInRad, vec3(0, 0, 1));
+		Transform F20(R20, d20); // transform from frame 2 to world
+		setColor(vec3(0.5, 0, 0.5));
+		drawBox(F20);
 
-      // Question 1 - B3
-      vec3 d30 = vec3(5, 4, 2);
-      angleInRad = 90 * atkmath::PI / 180;
-      quat R30 = glm::angleAxis(angleInRad, vec3(0,0,1));
-      Transform F30(R30, d30); // transform from frame 3 to world
-      setColor(vec3(0.0, 0.5,0.5));
-      drawBox(F30);
+		// Question 1 - B3
+		vec3 d30 = vec3(5, 4, 2);
+		angleInRad = 90 * atkmath::PI / 180;
+		quat R30 = glm::angleAxis(angleInRad, vec3(0, 0, 1));
+		Transform F30(R30, d30); // transform from frame 3 to world
+		setColor(vec3(0.0, 0.5, 0.5));
+		drawBox(F30);
 
-      // Question 2
-      Transform F21 = F10.inverse()*F20; // transform from frame 2 to frame 1
-      vec3 value = F21.transformPoint(vec3(0));
-      if (!once) std::cout << "Position of b2 relative to b1 is " << value << std::endl;
+		// Question 2
+		Transform F21 = F20 * F10.inverse(); // transform from frame 2 to frame 1
+		vec3 value = F21.transformPoint(vec3(0));
+		if (!once) std::cout << "Position of b2 relative to b1 is " << value << std::endl;
 
-      // Question 3
-      Transform F32 = F20.inverse()*F30; // transform from frame 3 to frame 2
-      value = F32.transformPoint(vec3(0));
-      if (!once) std::cout << "Position of b3 relative to b2 is " << value << std::endl;
+		// Question 3
+		Transform F32 = F30 * F20.inverse(); // transform from frame 3 to frame 2
+		value = F32.transformPoint(vec3(0));
+		if (!once) std::cout << "Position of b3 relative to b2 is " << value << std::endl;
 
-      // Question 4
-      // Compute a series of transforms that stack Box 2 onto Box 1
-      Transform transform(glm::angleAxis(0.f, vec3(1, 0, 0)), vec3(0, 2, 0));
-      Transform F = transform * F10 * F20.inverse();
+		// Question 4
+		// Compute a series of transforms that stack Box 2 onto Box 1
+		Transform transform(glm::angleAxis(0.f, vec3(1, 0, 0)), vec3(0, 2, 0));
+		Transform F = F10 * transform * F20.inverse();
 
-      setColor(vec3(0.5, 0.5,0.5));
-      drawBox(F * F20);
+		setColor(vec3(0.5, 0.5, 0.5));
+		drawBox(F * F20);
 
-      once = true;
-   }
+		once = true;
+	}
 
-   void drawBox(const Transform& xform)
-   {
-      vec3 size(3, 2, 1);
-      vec3 halfSize(1.5, 1.0, -0.5);
+	void drawBox(const Transform& xform)
+	{
+		vec3 size(3, 2, 1);
+		vec3 halfSize(1.5, 1.0, -0.5);
 
-      push();
-      scale(vec3(10));
-      transform(xform);
-      translate(halfSize);
-      drawCube(vec3(0), size);
-      pop();
+		push();
+		scale(vec3(10));
+		transform(xform);
+		translate(halfSize);
+		drawCube(vec3(0), size);
+		pop();
 
-      vec3 pos = 10.0f * xform.t();
-      setColor(vec3(1,0,0));
-      drawLine(pos, pos + xform.transformVector(vec3(10,0,0)));
+		vec3 pos = 10.0f * xform.t();
+		setColor(vec3(1, 0, 0));
+		drawLine(pos, pos + xform.transformVector(vec3(10, 0, 0)));
 
-      setColor(vec3(0,1,0));
-      drawLine(pos, pos + xform.transformVector(vec3(0,10,0)));
+		setColor(vec3(0, 1, 0));
+		drawLine(pos, pos + xform.transformVector(vec3(0, 10, 0)));
 
-      setColor(vec3(0,0,1));
-      drawLine(pos, pos + xform.transformVector(vec3(0,0,10)));
-   }
+		setColor(vec3(0, 0, 1));
+		drawLine(pos, pos + xform.transformVector(vec3(0, 0, 10)));
+	}
 
 private:
-  bool once = false;
+	bool once = false;
 };
 
 int main(int argc, char** argv)
 {
-   StackingBoxes viewer;
-   viewer.run();
-   return 0;
+	StackingBoxes viewer;
+	viewer.run();
+	return 0;
 }
--- assignments/a6-transform/skeleton_practice.cpp
+++ assignments/a6-transform/skeleton_practice.cpp
@@ -5,47 +5,47 @@ using glm::vec3;
 
 int main(int argc, char** argv)
 {
-   Skeleton skeleton;
-   Motion motion;
+	Skeleton skeleton;
+	Motion motion;
 
-   BVHReader reader;
-   reader.load("../motions/Beta/idle.bvh", skeleton, motion);
+	BVHReader reader;
+	reader.load("../motions/Beta/idle.bvh", skeleton, motion);
 
-   int numJoints = skeleton.getNumJoints();
-   std::cout << "The number of joints is " << numJoints << std::endl;
-   
-   
-   // todo: loop through and print all joint names
-   for (int i = 0; i < numJoints; i++)
-   {
-      // todo
-      std::string jointName = skeleton.getByID(i)->getName();
-      std::cout << i << " " << jointName << std::endl;
-   }
+	int numJoints = skeleton.getNumJoints();
+	std::cout << "The number of joints is " << numJoints << std::endl;
 
-   // todo get root of the skeleton
-   Joint* root = skeleton.getRoot();
-   std::string rootName = root->getName();
-   int rootID = root->getID();
-   int numchildren = root->getNumChildren();
-   std::cout << "The root joint name is: " << rootName << std::endl;
-   std::cout << "The root joint ID is: " << rootID << std::endl;
-   std::cout << "The root joint's number of children is: " << numchildren << std::endl;
 
-   // todo print the names of the children of the root
-   int rootNumChildren = numchildren;
-   for (int i = 0; i < rootNumChildren; i++)
-   {
-      std::string childName = root->getChildAt(i)->getName();
-      std::cout << childName << std::endl; 
-   } 
+	// todo: loop through and print all joint names
+	for (int i = 0; i < numJoints; i++)
+	{
+		// todo
+		std::string jointName = skeleton.getByID(i)->getName();
+		std::cout << i << " " << jointName << std::endl;
+	}
 
-   // Get the joint for the hand by looking up by name ("Beta:LeftHand")
-   Joint* hand = skeleton.getByName("Beta:LeftHand"); 
-   vec3 offset = hand->getLocalTranslation(); // todo get the local offset from the hand to its parent 
-   std::cout << "The offset between the left hand and its parent is " << glm::to_string(offset) << std::endl;
+	// todo get root of the skeleton
+	Joint* root = skeleton.getRoot();
+	std::string rootName = root->getName();
+	int rootID = root->getID();
+	int numchildren = root->getNumChildren();
+	std::cout << "The root joint name is: " << rootName << std::endl;
+	std::cout << "The root joint ID is: " << rootID << std::endl;
+	std::cout << "The root joint's number of children is: " << numchildren << std::endl;
 
-   // todo: get the hand's parent joint's name
-   std::string handParentName = hand->getParent()->getName();
-   std::cout << "The parent of the hand is " << handParentName << std::endl;
+	// todo print the names of the children of the root
+	int rootNumChildren = numchildren;
+	for (int i = 0; i < rootNumChildren; i++)
+	{
+		std::string childName = root->getChildAt(i)->getName();
+		std::cout << childName << std::endl;
+	}
+
+	// Get the joint for the hand by looking up by name ("Beta:LeftHand")
+	Joint* hand = skeleton.getByName("Beta:LeftHand");
+	vec3 offset = hand->getLocalTranslation(); // todo get the local offset from the hand to its parent 
+	std::cout << "The offset between the left hand and its parent is " << glm::to_string(offset) << std::endl;
+
+	// todo: get the hand's parent joint's name
+	std::string handParentName = hand->getParent()->getName();
+	std::cout << "The parent of the hand is " << handParentName << std::endl;
 }
