c22c09e19ea458c798e9203051e329344758aca1
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-11-12 16:52:09 -0500

    Part 4 and unique draft

--- assignments/a8-blend/reorient.cpp
+++ assignments/a8-blend/reorient.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
-#include "atkmath/constants.h"
 
 using namespace glm;
 using namespace atk;
@@ -13,128 +12,103 @@ using namespace atkui;
 class AReorient : public atkui::Framework
 {
 public:
-   AReorient() : atkui::Framework(atkui::Perspective) {}
-   virtual ~AReorient() {}
+    AReorient() : atkui::Framework(atkui::Perspective) {}
+    virtual ~AReorient() {}
 
-   void setup()
-   {
-      BVHReader reader;
-      reader.load("../motions/Beta/walking.bvh", _skeleton, _motion);
+    void setup()
+    {
+        BVHReader reader;
+        reader.load("../motions/Beta/walking.bvh", _skeleton, _motion);
 
-      _heading = 0;
-      _offset = vec3(0);
-      _offset[1] = _motion.getKey(0).rootPos[1];
-      _time = 0;
+        _heading = 0;
+        _offset = vec3(0);
+        _offset[1] = _motion.getKey(0).rootPos[1];
+        _time = 0;
 
-      _reoriented = reorient(_motion, _offset, _heading);
-   }
+        _reoriented = reorient(_motion, _offset, _heading);
+    }
 
-   Motion reorient(const Motion& motion, const vec3& pos, float heading)
-   {
-      Motion result;
-      result.setFramerate(motion.getFramerate());
+    Motion reorient(const Motion& motion, const vec3& pos, float heading)
+    {
+        Motion result;
+        result.setFramerate(motion.getFramerate());
 
-      // todo: your code here
-      // multiply the transform times the root position of our motion
-      for (int i = 0; i < motion.getNumKeys(); i++) {
-          Pose pose = motion.getKey(i);
-          Transform T1;
-          Transform T_desired(glm::angleAxis(heading, vec3(0, 1, 0)), pos);
-          Transform T_orig;
-          Transform T_new;
-          
-          if (i == 0) {
-             T1 = Transform (glm::angleAxis(0.f, vec3(0, 1, 0)), -pose.rootPos);
-             T_orig = T_desired;
-             T_new = T_desired * T1 * T_orig;
-             pose.rootPos = T_new.t();
-             pose.jointRots[i] = T_new.r();
+        for (int i = 0; i < motion.getNumKeys(); i++) {
+            Pose pose = motion.getKey(i);
+            result.appendKey(pose);
+        }
+        return result;
+    }
 
+    void update()
+    {
+        _time += dt() * 0.5;
+        _reoriented.update(_skeleton, _time);
+    }
 
-          }
-          else {
-              T_orig = Transform(pose.jointRots[_skeleton.getRoot()->getID()], pose.rootPos);
-              T_new = T_desired * T1 * T_orig;
-              pose.rootPos = T_new.t();
-              pose.jointRots[i] = T_new.r();
-          }
-          result.appendKey(pose);
-      }
-      
-      
-      return result;
-   }
+    void scene()
+    {
+        update();
+        SkeletonDrawer drawer;
+        drawer.draw(_skeleton, *this);
+    }
 
-   void update()
-   {
-      _time += dt() * 0.5;
-      _reoriented.update(_skeleton, _time);
-   }
+    void keyUp(int key, int mods)
+    {
+        if (key == GLFW_KEY_LEFT)
+        {
+            _heading += M_PI / 8;
+            _reoriented = reorient(_motion, _offset, _heading);
+            _time = 0;
+        }
+        else if (key == GLFW_KEY_RIGHT)
+        {
+            _heading -= M_PI / 8;
+            _reoriented = reorient(_motion, _offset, _heading);
+            _time = 0;
+        }
 
-   void scene()
-   {  
-      update();
-      SkeletonDrawer drawer;
-      drawer.draw(_skeleton, *this);
-   }
+        if (key == 'W')
+        {
+            _offset[2] += 25;
+            _reoriented = reorient(_motion, _offset, _heading);
+            std::cout << _offset << std::endl;
+            _time = 0;
+        }
+        else if (key == 'S')
+        {
+            _offset[2] -= 25;
+            _reoriented = reorient(_motion, _offset, _heading);
+            _time = 0;
+            std::cout << _offset << std::endl;
+        }
+        else if (key == 'A')
+        {
+            _offset[0] += 25;
+            _reoriented = reorient(_motion, _offset, _heading);
+            _time = 0;
+            std::cout << _offset << std::endl;
+        }
+        else if (key == 'D')
+        {
+            _offset[0] -= 25;
+            _reoriented = reorient(_motion, _offset, _heading);
+            _time = 0;
+            std::cout << _offset << std::endl;
+        }
+    }
 
-   void keyUp(int key, int mods) 
-   {
-      if (key == GLFW_KEY_LEFT)
-      {
-         _heading += atkmath::PI/8;
-         _reoriented = reorient(_motion, _offset, _heading);
-         _time = 0;
-      }
-      else if (key == GLFW_KEY_RIGHT)
-      {
-         _heading -= atkmath::PI/8;
-         _reoriented = reorient(_motion, _offset, _heading);
-         _time = 0;
-      }
-
-      if (key == 'W')
-      {
-         _offset[2] += 25;
-         _reoriented = reorient(_motion, _offset, _heading);
-         std::cout << _offset << std::endl;
-         _time = 0;
-      }
-      else if (key == 'S')
-      {
-         _offset[2] -= 25;
-         _reoriented = reorient(_motion, _offset, _heading);
-         _time = 0;
-         std::cout << _offset << std::endl;
-      }
-      else if (key == 'A')
-      {
-         _offset[0] += 25;
-         _reoriented = reorient(_motion, _offset, _heading);
-         _time = 0;
-         std::cout << _offset << std::endl;
-      }
-      else if (key == 'D')
-      {
-         _offset[0] -= 25;
-         _reoriented = reorient(_motion, _offset, _heading);
-         _time = 0;
-         std::cout << _offset << std::endl;
-      }
-   }
-
-   Skeleton _skeleton;
-   Motion _motion;
-   Motion _reoriented;
-   vec3 _offset;
-   float _heading;
-   float _time;
+    Skeleton _skeleton;
+    Motion _motion;
+    Motion _reoriented;
+    vec3 _offset;
+    float _heading;
+    float _time;
 };
 
 int main(int argc, char** argv)
 {
-   AReorient viewer;
-   viewer.run();
-   return 0;
+    AReorient viewer;
+    viewer.run();
+    return 0;
 }
-
--- assignments/a8-blend/unique.cpp
+++ assignments/a8-blend/unique.cpp
@@ -1,7 +1,183 @@
+#include "atk/toolkit.h"
 #include "atkui/framework.h"
+#include "atkui/skeleton_drawer.h"
+#include <algorithm>
+#include <string>
+
+using namespace atk;
+using namespace atkui;
 using namespace glm;
 
-int main(int argc, char** argv) {
-   return 0;
+class Unique : public atkui::Framework
+{
+public:
+    Unique() : atkui::Framework(atkui::Perspective) {}
+    virtual ~Unique() {}
+
+    void setup()
+    {
+        _alpha = 0;
+
+        BVHReader reader;
+        reader.load("../motions/14/14_01.bvh", _skeleton, _lower);
+        reader.load("../motions/14/14_37.bvh", _skeleton, _upper);
+        for (int i = _upper.getNumKeys() - 1; i >= 0; i--) {
+            _upper.appendKey(_upper.getKey(i));
+        }
+
+        _splice = spliceUpperBody(_lower, _upper, _alpha);
+
+
+        for (int i = 0; i < 3; i++) {
+            Skeleton _flower;
+            Joint* root_3 = new Joint("root_3");
+            root_3->setLocalTranslation(vec3(getPosition(i)[0], getPosition(i)[1], getPosition(i)[2]));
+            _flower.addJoint(root_3);
+
+            Joint* jf_1 = new Joint("jf_1");
+            jf_1->setLocalTranslation(vec3(0, 30, 30));
+            jf_1->setLocalRotation(glm::angleAxis(10.f, vec3(0, 1, 0)));
+            _flower.addJoint(jf_1, root_3);
+
+
+
+            _flower.fk();
+            _flowers.push_back(_flower);
+
+        }
+    }
+
+    Motion spliceUpperBody(const Motion& lower, const Motion& upper, float alpha)
+    {
+        Motion result;
+        result.setFramerate(lower.getFramerate());
+        // todo: your code here
+        Joint* startUpper = _skeleton.getByName("chest");
+
+
+
+        for (int i = 0; i < lower.getNumKeys(); i++) {
+            Pose pose = lower.getKey(i);
+            Pose newPose = pose;
+            for (int j = 0; j < pose.jointRots.size(); j++) {
+                if (isADescendant(startUpper, _skeleton.getByID(j))) {
+                    newPose.jointRots[j] = glm::slerp(upper.getKey(i%upper.getNumKeys()).jointRots[j], pose.jointRots[j], 0.0f);
+                }
+
+            }
+            result.appendKey(newPose);
+
+        }
+
+        return result;
+    }
+
+    void scene()
+    {
+        _splice.update(_skeleton, elapsedTime() * 0.5);
+        setColor(vec3(randValue(), randValue(), randValue()));
+        SkeletonDrawer drawer;
+        drawer.draw(_skeleton, *this);
+        //drawText("alpha: " + std::to_string(_alpha), 10, 15);
+       
+
+
+        for (int i = 0; i < 3; i++) {
+
+            setColor(vec3(1, 0, 1));
+
+            Skeleton _flower = _flowers[i];
+
+            _flower.fk();
+            for (int j = 0; j < _flower.getNumJoints(); j++) {
+
+                if (_flower.getByID(j) == _flower.getRoot()) {
+                    continue;
+                }
+              
+                Joint* parent = _flower.getByID(j)->getParent();
+                parent->setLocalRotation(glm::angleAxis(sin(elapsedTime()), vec3(0, 1, 0)));
+               
+
+                Joint* child = _flower.getByID(j);
+                child->setLocalRotation(glm::angleAxis(sin(elapsedTime()), vec3(0, 1, 0)));
+
+                for (int k = 0; k <= 500; k++) {
+
+                    float r = 30 * cos((5 * k));
+                    float x = r * cos(k);
+                    float y = r * sin(k) + 100;
+
+                    drawSphere(vec3(x, y, -100) + getPosition(i), 10);
+                }
+
+
+            }
+        }
+
+    }
+
+    bool isADescendant(Joint* parent, Joint* child) {
+        //int id = child.getID();
+        Joint* j = child;
+
+        while (j != NULL) {
+            if (j->getName() == parent->getName()) {
+                return true;
+            }
+            j = j->getParent();
+        }
+        return false;
+    }
+
+    void keyUp(int key, int mods)
+    {
+        if (key == GLFW_KEY_UP)
+        {
+            _alpha = std::min(_alpha + 0.05, 1.0);
+            _splice = spliceUpperBody(_lower, _upper, _alpha);
+            
+        }
+        else if (key == GLFW_KEY_DOWN)
+        {
+            _alpha = std::max(_alpha - 0.05, 0.0);
+            _splice = spliceUpperBody(_lower, _upper, _alpha);
+        }
+    }
+
+    virtual float randValue() {
+        float randValue = 0 + static_cast <float> (rand()) /
+            (static_cast <float> (RAND_MAX / (1 + 1)));
+        return randValue;
+    }
+
+    virtual vec3 getPosition(int i) {
+        Joint* head = _skeleton.getByName("head");
+        Joint* rHand = _skeleton.getByName("rHand");
+        Joint* lHand = _skeleton.getByName("lHand");
+
+
+        vec3 v;
+        if (i == 0) { v = head->getGlobalTranslation(); }
+        else if (i == 1) { v = rHand->getGlobalTranslation(); }
+        else { v = lHand->getGlobalTranslation(); }
+        return v;
+
+    }
+
+    Skeleton _skeleton;
+    Motion _upper;
+    Motion _lower;
+    Motion _splice;
+    float _alpha;
+    std::vector<Skeleton> _flowers;
+};
+
+int main(int argc, char** argv)
+{
+    Unique viewer;
+    viewer.run();
+    return 0;
 }
+
 
