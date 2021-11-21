8f290932f0dc9765d78cf30d554804c51b00075c
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-11-20 22:55:04 -0500

    part 1

--- assignments/a9-fade/crossfade.cpp
+++ assignments/a9-fade/crossfade.cpp
@@ -11,147 +11,137 @@ using namespace glm;
 class ACrossfadeViewer : public atkui::Framework
 {
 public:
-  ACrossfadeViewer() : atkui::Framework(atkui::Perspective) {}
-  virtual ~ACrossfadeViewer() {}
+	ACrossfadeViewer() : atkui::Framework(atkui::Perspective) {}
+	virtual ~ACrossfadeViewer() {}
 
-  void loadMotion1(const std::string &filename)
-  {
-    BVHReader reader;
-    reader.load(filename, skeleton_, motion1_);
-  }
+	void loadMotion1(const std::string& filename)
+	{
+		BVHReader reader;
+		reader.load(filename, skeleton_, motion1_);
+	}
 
-  void loadMotion2(const std::string &filename)
-  {
-    BVHReader reader;
-    reader.load(filename, skeleton_, motion2_);
-  }
+	void loadMotion2(const std::string& filename)
+	{
+		BVHReader reader;
+		reader.load(filename, skeleton_, motion2_);
+	}
 
-  void crossfade(int numBlendFrames)
-  {
-    assert(motion1_.getNumKeys() > 0);
-    assert(motion2_.getNumKeys() > 0);
-    assert(numBlendFrames > 0);
-    assert(numBlendFrames <= motion1_.getNumKeys());
-    assert(numBlendFrames <= motion2_.getNumKeys());
+	void crossfade(int numBlendFrames)
+	{
+		assert(motion1_.getNumKeys() > 0);
+		assert(motion2_.getNumKeys() > 0);
+		assert(numBlendFrames > 0);
+		assert(numBlendFrames <= motion1_.getNumKeys());
+		assert(numBlendFrames <= motion2_.getNumKeys());
 
-    blend_.setFramerate(motion1_.getFramerate());
-    int start1 = motion1_.getNumKeys() - numBlendFrames - 1;
-    int start2 = 0;
+		blend_.setFramerate(motion1_.getFramerate());
+		int start1 = motion1_.getNumKeys() - numBlendFrames - 1;
+		int start2 = 0;
 
-    reorient();
+		reorient();
 
-    for (int i = 0; i < start1; i++) {
-        blend_.appendKey(motion1_.getKey(i));
-    }
+		for (int i = 0; i < start1; i++) {
+			blend_.appendKey(motion1_.getKey(i));
+		}
 
-    motionsBlend(numBlendFrames, start1, start2);
+		motionsBlend(numBlendFrames, start1, start2);
 
-    for (int j = numBlendFrames; j < motion2_.getNumKeys(); j++) {
-        blend_.appendKey(motion2_.getKey(j));
-    }
+		for (int j = numBlendFrames; j < motion2_.getNumKeys(); j++) {
+			blend_.appendKey(motion2_.getKey(j));
+		}
 
+	}
 
-  
+	void motionsBlend(int numBlendFrames, int start1, int start2) {
 
-
-   
-  }
-
-  void motionsBlend(int numBlendFrames, int start1, int start2) {
-
-      for (float k = 0; k <= numBlendFrames; k ++) {
-          float alpha = (float)k / numBlendFrames;
-          Pose pose1 = motion1_.getKey(start1 + k);
-          Pose pose2 = motion2_.getKey(start2 + k);
-          Pose newPose = Pose::Lerp(pose1, pose2, alpha);
-          blend_.appendKey(newPose);
-          
-         
-      }
-      
-  }
-
-  void reorient() {
+		for (float k = 0; k <= numBlendFrames; k++) {
+			float alpha = (float)k / numBlendFrames;
+			Pose pose1 = motion1_.getKey(start1 + k);
+			Pose pose2 = motion2_.getKey(start2 + k);
+			Pose newPose = Pose::Lerp(pose1, pose2, alpha);
+			blend_.appendKey(newPose);
 
 
-     
+		}
 
-      Transform T1 = Transform(motion2_.getKey(0).jointRots[skeleton_.getRoot()->getID()], motion2_.getKey(0).rootPos);
-      Transform T_desired = Transform(motion1_.getKey(motion1_.getNumKeys() - 1).jointRots[skeleton_.getRoot()->getID()], motion1_.getKey(motion1_.getNumKeys() - 1).rootPos);
-      Transform T_offset = T_desired * T1.inverse();
-      for (int i = 0; i < motion2_.getNumKeys(); i++) {
-          Pose pose = motion2_.getKey(i);
-          Transform T_orig = Transform(pose.jointRots[skeleton_.getRoot()->getID()], pose.rootPos);
-          Transform T_new = T_offset * T_orig;
-          pose.rootPos = T_new.t();
-          pose.jointRots[skeleton_.getRoot()->getID()] = T_new.r();
-          motion2_.editKey(i, pose);
+	}
+
+	void reorient() {
 
 
-          
+		Transform T1 = Transform(motion2_.getKey(0).jointRots[skeleton_.getRoot()->getID()], motion2_.getKey(0).rootPos);
+		Transform T_desired = Transform(motion1_.getKey(motion1_.getNumKeys() - 1).jointRots[skeleton_.getRoot()->getID()], motion1_.getKey(motion1_.getNumKeys() - 1).rootPos);
+		Transform T_offset = T_desired * T1.inverse();
+		for (int i = 0; i < motion2_.getNumKeys(); i++) {
+			Pose pose = motion2_.getKey(i);
+			Transform T_orig = Transform(pose.jointRots[skeleton_.getRoot()->getID()], pose.rootPos);
+			Transform T_new = T_offset * T_orig;
+			pose.rootPos = T_new.t();
+			pose.jointRots[skeleton_.getRoot()->getID()] = T_new.r();
+			motion2_.editKey(i, pose);
 
-      }
-  }
+		}
+	}
 
-  void save(const std::string &filename)
-  {
-    BVHWriter writer;
-    writer.save(filename, skeleton_, blend_);
-  }
+	void save(const std::string& filename)
+	{
+		BVHWriter writer;
+		writer.save(filename, skeleton_, blend_);
+	}
 
-  void scene()
-  {
-    blend_.update(skeleton_, elapsedTime() * 0.5f);
-    drawer_.draw(skeleton_, *this);
-  }
+	void scene()
+	{
+		blend_.update(skeleton_, elapsedTime());
+		drawer_.draw(skeleton_, *this);
+	}
 
 private:
-  Skeleton skeleton_;
-  SkeletonDrawer drawer_;
-  Motion motion1_;
-  Motion motion2_;
-  Motion blend_;
+	Skeleton skeleton_;
+	SkeletonDrawer drawer_;
+	Motion motion1_;
+	Motion motion2_;
+	Motion blend_;
 };
 
-std::string PruneName(const std::string &name)
+std::string PruneName(const std::string& name)
 {
-  int idx1 = name.find_last_of("/");
-  int idx2 = name.find_last_of(".");
-  return name.substr(idx1 + 1, idx2 - idx1 - 1);
+	int idx1 = name.find_last_of("/");
+	int idx2 = name.find_last_of(".");
+	return name.substr(idx1 + 1, idx2 - idx1 - 1);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  std::string motion1 = "../motions/Beta/walking.bvh";
-  std::string motion2 = "../motions/Beta/jump.bvh";
-  int numFrames = 10;
+	std::string motion1 = "../motions/Beta/walking.bvh";
+	std::string motion2 = "../motions/Beta/jump.bvh";
+	int numFrames = 10;
 
-  try
-  {
-    for (int i = 0; i < argc; i++)
-    {
-      if (strncmp(argv[i], "-m1", 3) == 0)
-        motion1 = argv[i + 1];
-      else if (strncmp(argv[i], "-m2", 3) == 0)
-        motion2 = argv[i + 1];
-      else if (strncmp(argv[i], "-nframes", 7) == 0)
-        numFrames = std::stoi(argv[i + 1]);
-    }
-  }
-  catch (const std::exception &e)
-  {
-    std::cout << "usage: crossfade -m1 <path> -m2 <path> -nframes <num frames>\n";
-  }
+	try
+	{
+		for (int i = 0; i < argc; i++)
+		{
+			if (strncmp(argv[i], "-m1", 3) == 0)
+				motion1 = argv[i + 1];
+			else if (strncmp(argv[i], "-m2", 3) == 0)
+				motion2 = argv[i + 1];
+			else if (strncmp(argv[i], "-nframes", 7) == 0)
+				numFrames = std::stoi(argv[i + 1]);
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "usage: crossfade -m1 <path> -m2 <path> -nframes <num frames>\n";
+	}
 
-  ACrossfadeViewer viewer;
-  viewer.loadMotion1(motion1);
-  viewer.loadMotion2(motion2);
-  viewer.crossfade(numFrames);
+	ACrossfadeViewer viewer;
+	viewer.loadMotion1(motion1);
+	viewer.loadMotion2(motion2);
+	viewer.crossfade(numFrames);
 
-  std::string saveName = "blend-" + PruneName(motion1) + PruneName(motion2);
-  saveName += "-" + std::to_string(numFrames) + ".bvh";
-  viewer.save(saveName);
+	std::string saveName = "blend-" + PruneName(motion1) + PruneName(motion2);
+	saveName += "-" + std::to_string(numFrames) + ".bvh";
+	viewer.save(saveName);
 
-  viewer.run();
-  return 0;
+	viewer.run();
+	return 0;
 }
